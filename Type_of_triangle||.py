e5a38da32338eafecb4c88c16d5841929a6f0b84
srinidhiseethala <119839619+srinidhiseethala@users.noreply.github.com>
2024-02-04 23:55:04 +0530

    Create Type_of_triangle||.py

---
+++ Type_of_triangle||.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def triangleType(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: str
+        """
+        if(nums[0]+nums[1]>nums[2] and nums[1]+nums[2]>nums[0] and nums[2]+nums[0]>nums[1]):
+            if nums[0]==nums[1] and nums[1]==nums[2] and nums[2]==nums[1]:
+                return "equilateral"
+            elif nums[0]!=nums[1] and nums[1]!=nums[2] and nums[2]!=nums[0]:
+                return "scalene"
+            else:
+                return "isosceles"
+        else:
+            return "none"
