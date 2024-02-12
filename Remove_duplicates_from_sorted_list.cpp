75e7e30c116c8933661a3ce7d9cd9e1e3cd0d198
srinidhiseethala <119839619+srinidhiseethala@users.noreply.github.com>
2024-02-12 23:46:57 +0530

    Create Remove_duplicates_from_sorted_list.cpp

---
+++ Remove_duplicates_from_sorted_list.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* temp=head;
+        if(temp==NULL)
+        {
+            return head;
+        }
+        while(temp->next!=NULL)
+        {
+            if(temp->val==temp->next->val)
+            {
+                temp->next=temp->next->next;
+            }
+            else
+            {
+                temp=temp->next;
+            }
+        }
+        return head;
+    }
+};
