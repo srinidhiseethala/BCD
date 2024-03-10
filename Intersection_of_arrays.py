122e06feae9cf4051ec04bd403d4724d682a173c
srinidhiseethala <119839619+srinidhiseethala@users.noreply.github.com>
2024-03-10 19:50:05 +0530

    Create Intersection_of_arrays.py

---
+++ Intersection_of_arrays.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        l=[]
+        for i in nums1:
+            if i in nums2:
+                if i not in l:
+                    l.append(i)
+                else:
+                    continue
+        return l
