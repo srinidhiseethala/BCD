2dee81153a6500766d4775cd34fb9e46431cc031
srinidhiseethala <119839619+srinidhiseethala@users.noreply.github.com>
2024-03-31 08:33:28 +0530

    Create Harshad_Number.py

---
+++ Harshad_Number.py
@@ -0,0 +1,9 @@
+def sumOfTheDigitsOfHarshadNumber(self, x: int) -> int:
+        a=str(x)
+        s=0
+        for i in range(len(a)):
+            s+=int(a[i])
+        if x%s==0:
+            return x
+        else:
+            return -1
