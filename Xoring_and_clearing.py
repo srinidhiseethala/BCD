cae22d1c23091ab16dd04bfe373a516729bcdf6b
srinidhiseethala <119839619+srinidhiseethala@users.noreply.github.com>
2024-04-14 17:06:14 +0530

    Create Xoring_and_clearing.py

---
+++ Xoring_and_clearing.py
@@ -0,0 +1,16 @@
+def printArr(self, n, arr):
+        # printing array elements with spaces
+        for i in range(len(arr)):
+            print(arr[i],end=" ")
+        print()
+
+    def setToZero(self, n, arr):
+        # setting all elements to zero
+        for i in range(len(arr)):
+            arr[i]=0
+        
+    def xor1ToN(self, n, arr):
+        # doing xor with indices
+
+        for i in range(len(arr)):
+            arr[i]=arr[i]^i
